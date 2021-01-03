01c3f9db0fc579849b8b110ef1b49ef09d7dcad0
BMZOOO <76647795+BMZOOO@users.noreply.github.com>
2021-01-03 18:56:24 +0300

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Batool  Jawaheri  on 1/3/21.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
